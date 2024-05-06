cb8ca3c4ca273153d90c1980db71dc020cd65c29
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-05-06 20:32:53 +0800

    Add capture

--- STM32F103C8T6/main.c
+++ STM32F103C8T6/main.c
@@ -3,22 +3,26 @@
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_tim.h"
 
+#include <stdlib.h>
+
+#include "capture.h"
 #include "compare.h"
-#include "delay.h"
 #include "gpio.h"
-#include "motor.h"
+#include "oled.h"
 #include "pwm.h"
 #include "tim.h"
 
 uint16_t counter;
 
 int main() {
-    TIM tim = {
+    OLED_Init();
+
+    TIM tim2 = {
         RCC_APB1Periph_TIM2, TIM2, TIM_InternalClock, 720 - 1, 100 - 1,
     };
-    Compare compare = {
+    Compare compare1 = {
         TIM2,
-        0,
+        50,
         TIM_OC1Init,
         TIM_SetCompare1,
     };
@@ -29,33 +33,44 @@ int main() {
         GPIO_Mode_AF_PP,
     };
     PWM pwm = {
-        &tim,
-        &compare,
+        &tim2,
+        &compare1,
         &gpio_pwm,
     };
-    GPIO gpio_direction1 = {
+    PWM_Init(&pwm);
+
+    GPIO gpio_capture = {
         RCC_APB2Periph_GPIOA,
         GPIOA,
-        GPIO_Pin_1,
-        GPIO_Mode_Out_PP,
+        GPIO_Pin_6,
+        GPIO_Mode_IPU,
     };
-    GPIO gpio_direction2 = {
-        RCC_APB2Periph_GPIOA,
-        GPIOA,
-        GPIO_Pin_2,
-        GPIO_Mode_Out_PP,
+    TIM tim3 = {
+        RCC_APB1Periph_TIM3, TIM3, TIM_InternalClock, 72 - 1, 65536 - 1,
     };
-    Motor motor = {
-        &pwm,
-        &gpio_direction1,
-        &gpio_direction2,
+    Capture freq = {
+        TIM3, TIM_Channel_1,   TIM_ICPolarity_Rising, TIM_ICSelection_DirectTI,
+        0xF,  TIM_GetCapture1,
     };
-    Motor_Init(&motor);
+    Capture duty = {
+        TIM3,
+        TIM_Channel_2,
+        TIM_ICPolarity_Falling,
+        TIM_ICSelection_IndirectTI,
+        0xF,
+        TIM_GetCapture2,
+    };
+
+    GPIO_Init_(&gpio_capture);
+    TIM_Init(&tim3, NULL);
+    Capture_Init(&freq);
+    Capture_Init(&duty);
+
+    TIM_SelectInputTrigger(freq.TIMx, TIM_TS_TI1FP1);
+    TIM_SelectSlaveMode(freq.TIMx, TIM_SlaveMode_Reset);
 
     for (;;) {
-        for (int i = -100; i <= 100; i++) {
-            Motor_SetSpeed(&motor, i);
-            Delay_ms(10);
-        }
+        OLED_ShowNum(1, 1, Capture_GetFreq(&freq), 6);
+        OLED_ShowNum(2, 1, Capture_GetDuty(&freq, &duty), 3);
     }
 }
