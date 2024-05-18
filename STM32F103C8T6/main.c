900684b5d6714c9472ef813cdba1ee2f279894dd
LHYLiuWilliam <LHYLiuWilliam@gmail.com>
2024-05-18 12:57:44 +0800

    Add DMP

--- STM32F103C8T6/main.c
+++ STM32F103C8T6/main.c
@@ -5,12 +5,17 @@
 
 #include <stdlib.h>
 
+#include "delay.h"
+#include "dmp.h"
 #include "gpio.h"
 #include "i2c.h"
 #include "mpu.h"
 #include "rtc.h"
 #include "serial.h"
 #include "usart.h"
+
+I2C *GlobalI2C;
+Serial *GlobalSerial;
 
 int main() {
     GPIO gpio_TX = {
@@ -29,7 +34,9 @@ int main() {
         NULL,
         &usart,
     };
+    GlobalSerial = &serial;
     Serial_Init(&serial);
+    Serial_SendString(&serial, "\r\nSerial started\r\n");
 
     GPIO SCL = {
         RCC_APB2Periph_GPIOB,
@@ -48,30 +55,47 @@ int main() {
         I2C2,
         50000,
     };
+    GlobalI2C = &i2c;
     MPU mpu = {
         &SCL,
         &SDA,
         &i2c,
         MPU6050_DEVICE_ADDRESS,
     };
+    Serial_SendString(&serial, "\r\nstarting MPU\r\n");
     MPU_Init(&mpu);
+    Serial_SendString(&serial, "MPU started\r\n");
 
-    float roll, pitch;
+    Serial_SendString(&serial, "\r\nstarting RTC\r\n");
+    RTC_Init();
+    Serial_SendString(&serial, "RTC started\r\n");
+
+    Serial_SendString(&serial, "\r\nstarting DMP\r\n");
+    DMP_Init();
+    Serial_SendString(&serial, "DMP started\r\n");
+
+    float roll = 0, pitch = 0, yaw = 0;
     int16_t xacc, yacc, zacc, xgyro, ygyro, zgyro;
     int16_t xacc_offset, yacc_offset, xgyro_offset, ygyro_offset, zgyro_offset;
 
-    MPU_AdaptOffset(&mpu, 256, &xacc_offset, &yacc_offset, &xgyro_offset,
-                    &ygyro_offset, &zgyro_offset);
+    // Serial_SendString(&serial, "\r\nAdapting offset\r\n");
+    // MPU_AdaptOffset(&mpu, 256, &xacc_offset, &yacc_offset, &xgyro_offset,
+    //                 &ygyro_offset, &zgyro_offset);
+    // Serial_SendString(&serial, "offset adapted\r\n");
 
-    RTC_Init();
     for (;;) {
-        MPU_GetData(&mpu, &xacc, &yacc, &zacc, &xgyro, &ygyro, &zgyro);
-        MPU_Kalman(&mpu, &roll, &pitch, xacc, yacc, zacc, xgyro, ygyro, zgyro);
+        Delay_ms(100);
+        // MPU_GetData(&mpu, &xacc, &yacc, &zacc, &xgyro, &ygyro, &zgyro);
 
-        Serial_SendString(&serial, "%+6d %+6d %+6d %+6d %+6d %+6d ",
-                          xacc - xacc_offset, yacc - yacc_offset, zacc,
-                          xgyro - xgyro_offset, ygyro - ygyro_offset,
-                          zgyro - zgyro_offset);
-        Serial_SendString(&serial, "roll:%+8.2f pitch:%+8.2f\r\n", roll, pitch);
+        // Serial_SendString(&serial, "%+6d %+6d %+6d %+6d %+6d %+6d\r",
+        //                   xacc - xacc_offset, yacc - yacc_offset, zacc,
+        //                   xgyro - xgyro_offset, ygyro - ygyro_offset,
+        //                   zgyro - zgyro_offset);
+        DMP_GetData(&pitch, &roll, &yaw);
+        Serial_SendString(&serial, "pitch:%+8.2f roll:%+8.2f yaw:%+8.2f\r",
+                          pitch, roll, yaw);
+
+        // MPU_Kalman(&mpu, &roll, &pitch, xacc, yacc, zacc, xgyro, ygyro,
+        // zgyro);
     }
 }
